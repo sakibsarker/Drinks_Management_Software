8ca36063f129eaab2dc3b032d41a04914a98d7ef
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2022-11-17 14:13:35 +0600

    Profile Component

---
+++ src/component/Profile.component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Profile.style.css'
+function Profile() {
+    return (
+        <div className='profile_container'>
+
+
+            <img className='profile_picture' src='https://somporka.com/upload/photos/2020/03/QlVQ6W5AqvhAtbsMZjUe_10_a13aa0b1f1672890f6772b61fddb6de0_avatar_full.png'
+                alt='profile'
+            />
+            <h1 className='profile_name'>Touhid Bhuiyan</h1>
+
+        </div>
+    )
+}
+
+export default Profile
---
+++ src/pages/Home.pages.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from '../component/Profile.component'
+
+function Home() {
+    return (
+        // Profile
+        <Profile />
+        // DrinkList
+        // Progress
+    )
+}
+
+export default Home
