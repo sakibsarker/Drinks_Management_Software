133d1b259a0d8683765f3f388674a25e8e487e04
sakisarker <sakib15-3824@diu.edu.bd>
2022-11-17 14:37:40 +0600

    Progress buttona and completed button

---
+++ src/component/Progress.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Progress.style.css'
+const Progress = () => {
+  return (
+    <div>
+        <button className='Btn'>Pending</button>
+        <button className='Btn'>Completed</button>
+    </div>
+  )
+}
+
+export default Progress
