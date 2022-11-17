d6477438fb361e421e4e039f70b07b42f9407bae
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2022-11-17 17:44:13 +0600

    Frontend Complete

--- src/component/Progress.component.js
+++ src/component/Progress.component.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './Progress.style.css'
 const Progress = () => {
   return (
-    <div>
-      <button className='Btn'>Pending</button>
-      <button className='Btn'>Completed</button>
+    <div className='button-container'>
+      <button className='Btn'>Process</button>
+      <button className='Btn'>Complete</button>
     </div>
   )
 }
