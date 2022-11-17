da7e9051fb125e74e9e68ae056583b43bcc2ecce
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2022-11-17 18:39:44 +0600

    Fixing

--- src/component/Profile.component.js
+++ src/component/Profile.component.js
@@ -4,15 +4,17 @@ import logo from '../assets/call.gif'
 function Profile() {
     return (
         <div className='profile_container'>
-            <img className='profile_picture' src='https://somporka.com/upload/photos/2020/03/QlVQ6W5AqvhAtbsMZjUe_10_a13aa0b1f1672890f6772b61fddb6de0_avatar_full.png'
-                alt='profile'
-            />
-            <div className='profile_textContainer'>
+            <div className='profile_info'>
+                <img className='profile_picture' src='https://somporka.com/upload/photos/2020/03/QlVQ6W5AqvhAtbsMZjUe_10_a13aa0b1f1672890f6772b61fddb6de0_avatar_full.png'
+                    alt='profile'
+                />
+                <div className='profile_textContainer'>
+                    <h3 className='profile_name'>Touhid Bhuiyan</h3>
+                    <p>Department Head</p>
 
-                <h1 className='profile_name'>Touhid Bhuiyan</h1>
-                <img className='profile_icon' src={logo} alt='calling' />
+                </div>
             </div>
-
+            <img className='profile_icon' src={logo} alt='calling' />
 
         </div>
     )
